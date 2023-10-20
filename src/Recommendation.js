70f4f141bbac4c3f2d4c3cc273d1ce9b7f535b01
mrsBarul <dobro.ve@mail.ru>
2023-10-20 03:55:27 +0300

    update rec

--- src/Recommendation.js
+++ src/Recommendation.js
@@ -6,7 +6,7 @@ function Recommendation() {
     const [activity, setActivity] = useState('');
 
     const getActivity = useCallback(async() => {
-        const response = await fetch('http://www.boredapi.com/api/activity/ ')
+        const response = await fetch('https://www.boredapi.com/api/activity/ ')
         const data = await response.json()
         setActivity(data.activity)
     }, [])
